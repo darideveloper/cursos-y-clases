83876d8baa53aec1ac50103141cd0c2b8a748f99
daridev <hernandezdarifrancisco@gmail.com>
2022-10-25 22:12:44 -0500

    clase 2 tarea

---
+++ Ernesto/clase_2/6_tarea.py
@@ -0,0 +1,15 @@
+"""
+La siguiente información se ha obtenido de una base de datos, por lo quen no la puedes modificar directamente.
+Con la listad e usuarios, realiza lo siguiente: 
+1. Crea una copia de las listas de usuarios, donde ningún elemento tenga texto adicional
+1. Imprime un mensaje de bienvenida a cada usuario, asegurandote de no imprimir texto en blanco y que la primer letra de cada nombre esté en mayúscula
+2. Añade a la lista de usuarios el usuario "admin" al inicio de la lista.
+3. Elimina a "juan" de la lista de usuarios.
+4. Copiar todos los usuarios a una nueva lista llamada "usuarios_sorted", donde los usuarios estén ordenados alfabéticamente.
+5. Imprime el número de usuarios que hay en la lista.
+6. Crear una nueva lista, llamada "contraseñas", donde cada usuario tenga una contraseña asociada.
+7. Imprime la contraseña que corresponde al usuario "josé" 
+
+"""
+
+usuarios = [" juan ", "  maria", "jorge ", "  josé "]
