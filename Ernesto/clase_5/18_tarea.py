73878d08bd8316d15f6dc59923401bcd2470474b
daridev <hernandezdarifrancisco@gmail.com>
2022-10-30 05:54:49 -0600

    clase 5 tarea

---
+++ Ernesto/clase_5/18_tarea.py
@@ -0,0 +1,11 @@
+"""Escribe un programa que encuentre el segundo elemento más grande dentro de una lista. Prueba
+tu programa con las siguientes listas: 
+lista1 = [8, -1, -2, 2, 0], 
+lista2 = [-6, -7, -8, -12, -4], 
+lista3 = [98, 0, -56, 64, 64] (10 pts)
+
+Ejemplo de salida: 
+
+El segundo elemento más grande en la lista1 es : 2
+El segundo elemento más grande en la lista2 es : -6
+El segundo elemento más grande en la lista2 es : 64"""
