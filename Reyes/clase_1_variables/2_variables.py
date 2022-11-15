aad444df1611316b62e19c370d3fa707028567ed
daridev <hernandezdarifrancisco@gmail.com>
2022-11-14 20:52:47 -0600

    reyes clase 1

---
+++ Reyes/clase_1_variables/2_variables.py
@@ -0,0 +1,12 @@
+# String / textos
+nombre = "Juan"
+
+# Int / enteros
+edad = 40
+
+# Float / decimales
+precio = 55.60
+
+print (nombre)
+print (edad)
+print (precio)
---
+++ Reyes/clase_1_variables/3_strings.py
@@ -0,0 +1,43 @@
+# COMILLAS
+
+texto1 = "Juan" # Comillas dobles
+texto2 = 'Juan' # Comillas simples
+texto3 = """Juan""" # Comillas triples
+
+
+# Comillas dobles dentro de texto
+cita = 'Albert Aiinstain dijo: "Todo debe simplificarse lo máximo posible, pero no más"'
+
+# Comillas simples dentro de texto
+message = "Ana's book"
+
+# Comillas dobles y simples dentro de texto
+parrafo = """Lorem''' ipsum d""o"lor sit amet, con"s""ectetur adipiscing elit. """
+
+print (cita)
+print (message)
+print (parrafo)
+
+# CASES
+
+nombre = "JuAn LoPeZ"
+print (nombre.upper()) # JUAN LOPEZ
+print (nombre.lower()) # juan lopez
+print (nombre.title()) # Juan Lopez
+
+# CANCATENAR (UNIR TEXTOS)
+
+nombre = "Alberto"
+apellido = "Martinez"
+print ("Bienvenido usuario " + nombre + " " + apellido)
+
+# CONCATENAR (STRING PREFORMATEADOS / F-STRINGS)
+nombre = "Alberto"
+apellido = "Lopez"
+print (f"Bienvenido usuario {nombre} {apellido}") 
+
+# CARACTERES ESPECIALES
+parrafo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit.\nLorem ipsum dolor sit amet, consectetur adipiscing elit."
+nombre = "\t\t\tJuan"
+print (parrafo)
+print (nombre)
