efb6c7367ac24a0dfa185021b3acf4eeca780349
daridev <hernandezdarifrancisco@gmail.com>
2022-11-14 23:34:07 -0600

    python 0 clase 10 desafíos

---
+++ Python desde 0 g1/clase_10_while_input_funciones/deafio.py
@@ -0,0 +1,17 @@
+"""
+Crea un menu con lac opciones:
+1. Enviar correo
+2. Leer correo
+3. Salir
+Cada una de las opciones del menu, debe ser una funcion
+
+* Enviar correo
+La función correspondiente al envio de correo, debes recibir como parametro el correo del destinatario
+Internamente, la función debe comprobar que el correo incluya el caracter "@", y un punto
+Si el correo es correcto, la función debe devolver en True, de lo contrario, debe devolver False
+
+* Leer correo
+La función no recibe parametros
+La función devuelve un texto aleatorio de una lista de textos, que siularán ser los correos recibidos
+Utiliza el siguiente código de rerferencia para obtener el texto aleatorio: https://pynative.com/python-random-choice/
+"""
