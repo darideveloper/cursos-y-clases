e6d7e4b8c7168361a868af95033a7486d500291f
darideveloper <darideveloper@gmail.com>
2023-03-26 19:42:26 -0600

    python 0 clase 8

--- Python desde 0 g2/17_condicionales.py
+++ Python desde 0 g2/17_condicionales.py
@@ -1,24 +1,39 @@
-#martin aca
-nume = 10
-if nume % 2 == 0:
-    print("es par")
+# Comprobar la edad e imprimir un mensaje
+edad = 150
+
+if edad < 10:
+    print ("Eres un niño")
+elif edad < 18:
+    print ("Eres un adolescente")
+elif edad < 50:
+    print ("Eres un adulto")
+elif edad < 100:
+    print ("Eres un anciano")
 else:
-    print("es impar")
+    print ("Eres un fantasma")
 
+print ("Final del programa")
 
+# Similar un inicio de sesión
 
+# Consultando usuarios de la DB
+users_db = ["daridev", "dari", "dariel", "darieldev"]
 
+# Leer datos de inicio de sesión
+user = "daridev"
+password = "1234dari123*+"
 
-
-
-
-
-
-
-# ///////Xavier Here
-
-edad = 25
-if edad >= 18:
-    print("Mayor de edad")
+# Comprobar el nombre de usuario
+if user in users_db:
+    
+    # Obtener contraseña del usuario actual
+    password_db = "1234dari123*+"
+    
+    # Comprobar contraseña
+    if password == password_db:
+        print ("Bienvenido")
+    else:
+        print ("Usuario o contraseña incorrectos")
+        
 else:
-    print("Menor de edad")
+    print ("Usuario o contraseña incorrectos")
---
+++ Python desde 0 g2/18_operadores.py
@@ -0,0 +1,26 @@
+# Sí, Verdadero
+running = True
+
+# No, Falso
+running = False
+
+# Comparaciones de textos
+texto_a = "Hola"
+texto_b = "hola"
+texto_a == texto_b # False # Comprar si son iguales
+texto_a != texto_b # True # Comprar si son diferentes
+texto_a.lower() == texto_b.lower() # True
+
+texto_a = "   mundo "
+texto_b = "mundo"
+texto_a == texto_b # False
+texto_a.strip() == texto_b.strip() # True
+
+# Comparaciones de números
+edad = 18
+edad == 18 # True # comparar si son iguales
+edad != 18 # False # comparar si son diferentes
+edad > 10 # True # comparar si es mayor
+edad < 10 # False # comparar si es menor
+edad >= 10 # True # comparar si es mayor o igual
+edad <= 10 # False # comparar si es menor o igual
