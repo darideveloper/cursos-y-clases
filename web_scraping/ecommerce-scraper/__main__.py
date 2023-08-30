ab252f9a716a976d6d934a224fe2c0278e33cb20
daridev <darideveloper@gmail.com>
2023-08-30 14:06:08 -0600

    web scraping: s4

--- web_scraping/ecommerce-scraper/__main__.py
+++ web_scraping/ecommerce-scraper/__main__.py
@@ -1,7 +1,15 @@
+import os
+from dotenv import load_dotenv
 from chrome_dev.chrome_dev import ChromDevWrapper
 
-MAX_PRODUCTS = 11
+# read .env file
+load_dotenv ()
+MAX_PRODUCTS = int(os.getenv ("MAX_PRODUCTS"))
+CHROME_PATH = os.getenv ("CHROME_PATH")
+
 START_PRODUCT_INDEX = 6
+
+print ("(Amazon) Searching products...")
 
 selectors = {
     'product': '[data-asin][data-uuid]',
@@ -11,27 +19,29 @@ selectors = {
     'reviews': 'span[aria-label]:nth-child(2)',
     'sponsored': '[aria-label~="Sponsored"]',
     'best_seller': '.a-row.a-badge-region',
-    'price': 'a .a-offscreen',
-    'last_month_sales': 'span.a-size-base.a-color-secondary',
+    'price': 'a.a-size-base .a-offscreen',
+    'last_month_sales': 'div > span.a-size-base.a-color-secondary:only-child',
     'link': 'a',
 }
 
 # Open chrome and load results page
-chrome_path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-scraper = ChromDevWrapper(chrome_path=chrome_path, start_killing=False)
+scraper = ChromDevWrapper(chrome_path=CHROME_PATH, start_killing=False)
 scraper.set_page ("https://www.amazon.com/s?k=protein&s=review-rank")
 
 # get the results in the page
-results = scraper.count_elems (selectors['product'])
+results_num = scraper.count_elems (selectors['product'])
 
 # Validate if there are results
-if not results:
+if results_num == 0:
     print ("No results found")
     quit ()
 
 current_index = START_PRODUCT_INDEX
 extracted_products = 0
 
+print ("(Amazon) Extracting data...")
+
+products_data = []
 while True:
     
     # Generate css selectors
@@ -46,59 +56,71 @@ while True:
     selector_last_month_sales = f'{selector_product} {selectors["last_month_sales"]}'
     selector_link = f'{selector_product} {selectors["link"]}'
     
+    # Incress product counter
+    current_index += 1
+    
+    # Validate if there are not more products in the page
+    if current_index - START_PRODUCT_INDEX > results_num:
+        print ("(Amazon) No more products")
+        break
+    
+    # Skip products without price
+    price = scraper.get_text (selector_price)
+    if not price:
+        continue
+    
+    # Skip sponsored products
+    sponsored = scraper.get_text (selector_sponsored)
+    if sponsored:
+        continue
+    
     # Extract text from selectors
     image = scraper.get_attrib (selector_image, "src")    
     title = scraper.get_text (selector_title)
     rate_num = scraper.get_text (selector_rate_num)
     reviews = scraper.get_attrib (selector_reviews, "aria-label")
-    sponsored = scraper.get_text (selector_sponsored)
     best_seller = scraper.get_text (selector_best_seller)
-    price = scraper.get_text (selector_price)
     last_month_sales = scraper.get_text (selector_last_month_sales)
     link = scraper.get_attrib (selector_link, "href")
+        
+    # Clean data 
+    price = float(price.replace("$", ""))
     
-    # Clean data
     if rate_num:
         rate_num = float(rate_num[0:3])
     
     if reviews:
         reviews = int(reviews.replace(",", ""))
         
-    if sponsored:
-        sponsored = True
-    else: 
-        sponsored = False
-        
     if best_seller:
-        sponsored = True
+        best_seller = True
     else:
-        sponsored = False
+        best_seller = False
         
     if last_month_sales:
         last_month_sales = last_month_sales.split(" ")[0]
-        
-    # TODO: skip products without price
-    if price:    
-        price = float(price.replace("$", ""))
     
     # TODO: add referral link
-    link = "https://www.amazon.com{link}"
-    
-    # Incress product counter
-    current_index += 1
+    link = f"https://www.amazon.com{link}"
     
     # Incress counter of extracted products
     extracted_products += 1
     
+    # Save data
+    products_data.append ({
+        "image": image, 
+        "title": title,
+        "rate_num": rate_num,
+        "reviews": reviews,
+        "price": price,
+        "best_seller": best_seller,
+        "last_month_sales": last_month_sales,
+        "link": link,
+    })
+    
+    
     # End loop when extract al required products
     if extracted_products >= MAX_PRODUCTS: 
         break
-    
-    # TODO: end loop of there isn't more products in the page
-    
-    
-
-
-
-
-print ("done")
+        
+print (f"(Amazon) {extracted_products} products extracted")
---
+++ web_scraping/ecommerce-scraper/test_requests.py
@@ -0,0 +1,3 @@
+import requests
+
+res = requests.get('http://www.gutenberg.org/cache/epub/1112/pg1112.txt')
